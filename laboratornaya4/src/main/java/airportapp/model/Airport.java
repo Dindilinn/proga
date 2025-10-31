98b5bd0b58d56873c1fb90b239888812a8e8cb4a
Dindilinn <dianakras60@gmail.com>
2025-10-31 10:05:12 +0300

    Update Airport.java

--- laboratornaya4/src/main/java/airportapp/model/Airport.java
+++ laboratornaya4/src/main/java/airportapp/model/Airport.java
@@ -47,26 +47,26 @@ public class Airport {
     }
 
     public void loadFromFile(String filename) throws IOException, InvalidTariffException {
-    tariffs.clear();
-    List<String> lines = Files.readAllLines(Paths.get(filename));
-    for (String line : lines) {
-        line = line.trim();
-        if (line.isEmpty()) continue;
-        String[] parts = line.split("\\|");
-        if (parts.length != 3) {
-            throw new InvalidTariffException("Некорректный формат строки: " + line);
+        tariffs.clear();
+        List<String> lines = Files.readAllLines(Paths.get(filename));
+        for (String line : lines) {
+            line = line.trim();
+            if (line.isEmpty()) continue;
+            String[] parts = line.split("\\|");
+            if (parts.length != 3) {
+                throw new InvalidTariffException("Некорректный формат строки: " + line);
+            }
+            String destination = parts[0];
+
+            // Заменяем запятую на точку для парсинга
+            String basePriceStr = parts[1].replace(',', '.');
+            String discountStr = parts[2].replace(',', '.');
+
+            double basePrice = Double.parseDouble(basePriceStr);
+            double discount = Double.parseDouble(discountStr);
+
+            DiscountStrategy strategy = (discount > 0) ? new FixedDiscount(discount) : new NoDiscount();
+            tariffs.add(new Tariff(destination, basePrice, strategy));
         }
-        String destination = parts[0];
-        
-        // Заменяем запятую на точку для парсинга
-        String basePriceStr = parts[1].replace(',', '.');
-        String discountStr = parts[2].replace(',', '.');
-
-        double basePrice = Double.parseDouble(basePriceStr);
-        double discount = Double.parseDouble(discountStr);
-
-        DiscountStrategy strategy = (discount > 0) ? new FixedDiscount(discount) : new NoDiscount();
-        tariffs.add(new Tariff(destination, basePrice, strategy));
     }
-}
 }
